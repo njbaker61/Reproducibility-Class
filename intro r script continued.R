b1dfc365e43218deede98882704d36f40f1d57da
Nicole Baker <nicolescomputer@Nicoles-MacBook-Pro.local>
2023-02-14 16:33:37 -0600

    last intro R script

---
+++ intro r script continued.R
@@ -0,0 +1,14 @@
+##Installing Packages: download and then upload
+
+install.packages("ggplot2")
+
+#can load into R by clicking the check box in the packages list, or:
+
+library(ggplot2)
+
+#reading in data: .csv, etc.
+read.csv("Lab8Data1/corr.csv",na.strings="NA")
+
+
+
+
