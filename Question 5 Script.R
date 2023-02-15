387a540ccc4eb77d80df5498a857194b189ebee9
Nicole Baker <nicolescomputer@Nicoles-MacBook-Pro.local>
2023-02-15 13:14:32 -0600

    answers to question 5 of the intro to R assignment

---
+++ Question 5 Script.R
@@ -0,0 +1,18 @@
+##installing packages
+install.packages("ggplot2")
+library(ggplot2)
+
+install.packages("dplyr")
+library(dplyr)
+
+install.packages("purrr")
+library(purrr)
+
+install.packages("lme4")
+library(lme4)
+
+install.packages("emmeans")
+library(emmeans)
+
+##all packages were confirmed installed with red text and the location of the downloaded package
+##the library function confirmed upload into R studio, and all packages had a check mark
