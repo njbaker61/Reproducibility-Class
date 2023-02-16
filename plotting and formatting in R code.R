99b15d9fe9cbc65895eb39296982f87eb9f6a3ab
Nicole Baker <nicolescomputer@Nicoles-MacBook-Pro.local>
2023-02-16 17:03:13 -0600

    new additions to ggplot basics

--- plotting and formatting in R code.R
+++ plotting and formatting in R code.R
@@ -30,3 +30,52 @@ ggplot(mtcars,aes(x=wt,y=mpg))+
   xlab("Weight") + ylab("Miles per Gallon") + #labels axes what you want
   theme_classic()  #can explore themes for graphs?
 
+
+##scales-size
+
+ggplot(mtcars,aes(x=wt,y=mpg))
+ggplot(mtcars,aes(x=wt,y=mpg))+
+  geom_point(aes(size=wt)) +  #here
+  geom_smooth(method=lm,se=FALSE)+
+  
+  xlab("Weight") + ylab("Miles per Gallon") + 
+  theme_classic() 
+
+##scales-color
+
+ggplot(mtcars,aes(x=wt,y=mpg))
+ggplot(mtcars,aes(x=wt,y=mpg))+
+  geom_point(aes(color=wt)) +  #here
+  geom_smooth(method=lm,se=FALSE)+
+  
+  xlab("Weight") + ylab("Miles per Gallon") + 
+  theme_classic() +
+  scale_color_gradient(low="forestgreen",high="blue") #here
+  
+##log transforming 
+
+ggplot(mtcars,aes(x=wt,y=mpg))
+ggplot(mtcars,aes(x=wt,y=mpg))+
+  geom_point(aes(color=wt)) +  
+  geom_smooth(method=lm,se=FALSE)+
+  
+  xlab("Weight") + ylab("Miles per Gallon") + 
+  theme_classic() +
+  scale_color_gradient(low="forestgreen",high="blue") +
+  scale_y_log10() #here
+
+
+
+##
+
+bull.richness<-read.csv("/Users/nicolescomputer/Documents/Reproducibility/research/Reproducibility/Repository/BullRichness.csv",na.strings=NA)
+##ill figure this out....
+
+bull.richness.soy.no.till<-BullRichness[BullRichness$Crop == "Soy" & 
+                                          BullRichness$Treatment=="No-till",]
+
+
+ggplot(bull.richness.soy.no.till, aes(x=GrowthStage,y=richness,color=Fungicide))+
+  geom_boxplot() +
+  geom_point(position = position_jitterdodge(dodge.width=0.9))
+
