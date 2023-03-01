be9f3a30d6b742b2d24cf2d379d42d1e09583e96
Nicole Baker <nicolescomputer@Nicoles-MacBook-Pro.local>
2023-03-01 11:11:52 -0600

    fixing my graphic outputs with the new mycotoxin data file

--- DataVisualizationAssignment.R
+++ DataVisualizationAssignment.R
@@ -3,11 +3,11 @@ download.packages("ggplot2")
 library(ggplot2)
 
 #loading file
-read.csv("MycotoxinData.csv",na.strings=".")
-MycotoxinData
+mycotoxin <- read.csv("MycotoxinData2.csv")
+str(mycotoxin)
 
 ##QUESTION 2: making box plot with y axis label
-ggplot(MycotoxinData,aes(x=Treatment,y=DON,color=Cultivar))+
+ggplot(mycotoxin,aes(x=Treatment,y=DON,color=Cultivar))+
   geom_boxplot()+
   geom_point(position=position_jitterdodge(dodge.width=0.9))+
   xlab("") + ylab("DON (ppm)")+
@@ -15,7 +15,7 @@ ggplot(MycotoxinData,aes(x=Treatment,y=DON,color=Cultivar))+
 #it isnt clear where my boxes for the box plot are...
 
 ##QUESTION 3: make this data a bar chart
-ggplot(MycotoxinData,aes(x=Treatment,y=DON,fill=Cultivar))+
+ggplot(mycotoxin,aes(x=Treatment,y=DON,fill=Cultivar))+
   stat_summary(fun=mean,geom="bar",position="dodge",color="black")+
   stat_summary(fun.data=mean_se,geom="errorbar",position="dodge")+
   scale_color_manual(values=c("pink","purple"))+
@@ -25,7 +25,7 @@ ggplot(MycotoxinData,aes(x=Treatment,y=DON,fill=Cultivar))+
 #the error bars are coded but I cant see them in my plot. maybe they are too small?
 
 ##QUESTION 4:add data points to foreground
-ggplot(MycotoxinData,aes(x=Treatment,y=DON,fill=Cultivar))+
+ggplot(mycotoxin,aes(x=Treatment,y=DON,fill=Cultivar))+
   stat_summary(fun=mean,geom="bar",position="dodge",color="black")+
   stat_summary(fun.data=mean_se,geom="errorbar",position="dodge")+
   geom_point(position=position_jitterdodge(dodge.width=1),shape=21,color="Black")+
@@ -35,7 +35,7 @@ ggplot(MycotoxinData,aes(x=Treatment,y=DON,fill=Cultivar))+
   theme_classic()
 
 ##QUESTION 5: match the color blind pallet
-ggplot(MycotoxinData,aes(x=Treatment,y=DON,fill=Cultivar))+
+ggplot(mycotoxin,aes(x=Treatment,y=DON,fill=Cultivar))+
   stat_summary(fun=mean,geom="bar",position="dodge",color="black")+
   stat_summary(fun.data=mean_se,geom="errorbar",position="dodge")+
   geom_point(position=position_jitterdodge(dodge.width=1),shape=21,color="Black")+
@@ -45,7 +45,7 @@ ggplot(MycotoxinData,aes(x=Treatment,y=DON,fill=Cultivar))+
   theme_classic()
 
 ##QUESTION 6: facet based on cultivar
-ggplot(MycotoxinData,aes(x=Treatment,y=DON,fill=Cultivar))+
+ggplot(mycotoxin,aes(x=Treatment,y=DON,fill=Cultivar))+
   stat_summary(fun=mean,geom="bar",position="dodge",color="black")+
   stat_summary(fun.data=mean_se,geom="errorbar",position="dodge")+
   geom_point(position=position_jitterdodge(dodge.width=1),shape=21,color="Black")+
@@ -56,7 +56,7 @@ ggplot(MycotoxinData,aes(x=Treatment,y=DON,fill=Cultivar))+
   theme_classic()
 
 ##QUESTION 7: choose a theme
-ggplot(MycotoxinData,aes(x=Treatment,y=DON,fill=Cultivar))+
+ggplot(mycotoxin,aes(x=Treatment,y=DON,fill=Cultivar))+
   stat_summary(fun=mean,geom="bar",position="dodge",color="black")+
   stat_summary(fun.data=mean_se,geom="errorbar",position="dodge")+
   geom_point(position=position_jitterdodge(dodge.width=1),shape=21,color="Black")+
@@ -68,7 +68,7 @@ ggplot(MycotoxinData,aes(x=Treatment,y=DON,fill=Cultivar))+
 #changed to dark theme
 
 ##QUESTION 8: add transparency to data points
-ggplot(MycotoxinData,aes(x=Treatment,y=DON,fill=Cultivar))+
+ggplot(mycotoxin,aes(x=Treatment,y=DON,fill=Cultivar))+
   stat_summary(fun=mean,geom="bar",position="dodge",color="black")+
   stat_summary(fun.data=mean_se,geom="errorbar",position="dodge")+
   geom_point(position=position_jitterdodge(dodge.width=1),shape=21,color="Black",
@@ -81,7 +81,7 @@ ggplot(MycotoxinData,aes(x=Treatment,y=DON,fill=Cultivar))+
 #i used the alpha function to reduce the point opacity
 
 ##QUESTION 10: explore plotting options
-ggplot(MycotoxinData,aes(x=Treatment,y=DON,color=Cultivar))+
+ggplot(mycotoxin,aes(x=Treatment,y=DON,color=Cultivar))+
   geom_count()+
   scale_size_area()+
   scale_color_manual(values=c("#CC79A7","#009E73"))+
@@ -92,7 +92,7 @@ ggplot(MycotoxinData,aes(x=Treatment,y=DON,color=Cultivar))+
 # I would use this for my data, since I have species counts
 
 
-ggplot(MycotoxinData,aes(x=Treatment,y=DON,color=Cultivar))+
+ggplot(mycotoxin,aes(x=Treatment,y=DON,color=Cultivar))+
   geom_point()+
   geom_rug()+
   scale_color_manual(values=c("#CC79A7","#009E73"))+
